d288b40cf2d92dd59f65a3668d96540a768cb1cf
trilochaan-dev <trilochaan42@gmail.com>
2025-07-31 02:23:14 +0530

    did most off the work

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function sidebar(){
+    document.getElementById('sidebar').classList.toggle('expand')
+    document.getElementById('toggle-arrow').classList.toggle('flip')
+    document.getElementById('arrow').classList.toggle('flip')
+    document.getElementById('main').classList.toggle('main-mini')
+}
+
