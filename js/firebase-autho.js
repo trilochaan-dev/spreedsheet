8df1a5b5c01a316c2491e2f3ed1f960a07617875
trilochaan-dev <trilochaan42@gmail.com>
2025-08-03 03:02:00 +0530

    fcsdcs

--- js/firebase-autho.js
+++ js/firebase-autho.js
@@ -1,17 +1,15 @@
-const auth = firebase.auth();
-  const provider = new firebase.auth.GoogleAuthProvider();
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
-  function signInWithGoogle() {
-    auth.signInWithPopup(provider)
-      .then((result) => {
-        const user = result.user;
-        alert("Signed in as: " + user.displayName);
-        // Save user data or redirect, etc.
-      })
-      .catch((error) => {
-        console.error("Sign-in error:", error);
-        alert("Error: " + error.message);
-      });
-  }
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-window.signInWithGoogle = signInWithGoogle;
+function signInWithGoogle() {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const user = result.user;
+      alert("Signed in as: " + user.displayName);
+    })
+    .catch((error) => {
+      console.error("Sign-in failed", error);
+    });
+}
